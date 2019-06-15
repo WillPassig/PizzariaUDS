a5d4e66cc7ead3d60cae489bd6f10856139ff4a8
Passig <wilpsouza@gmail.com>
2019-06-15 14:56:57 -0300

    Criada estrutura dentro de cada projeto (pastas, alguns arquivos ainda vazios) e feita pasta para projeto de testes iniciando com testes de domínio.

---
+++ PizzariaUDS/PizzariaUDS.Infra.CrossCutting.Bus/InMemoryBus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzariaUDS.Infra.CrossCutting.Bus
+{
+    class InMemoryBus
+    {
+    }
+}
---
+++ PizzariaUDS/PizzariaUDS.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzariaUDS.Infra.CrossCutting.IoC
+{
+    class NativeInjectorBootStrapper
+    {
+    }
+}
