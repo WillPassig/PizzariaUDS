75a56651c6e6a8d990a547d81347812ef618f76d
Passig <wilpsouza@gmail.com>
2019-06-16 14:33:10 -0300

    Concluída estrutura básica de Domain e Domain.Core

---
+++ PizzariaUDS/PizzariaUDS.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,12 @@
+﻿using PizzariaUDS.Domain.Core.Commands;
+using PizzariaUDS.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace PizzariaUDS.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        Task RaiseEvent<T>(T @event) where T : Event;
+    }
+}
---
+++ PizzariaUDS/PizzariaUDS.Domain/EventHandlers/OrderEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using PizzariaUDS.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PizzariaUDS.Domain.EventHandlers
+{
+    public class OrderEventHandler :
+        INotificationHandler<OrderRegisteredEvent>,
+        INotificationHandler<OrderUpdatedEvent>,
+        INotificationHandler<OrderRemovedEvent>
+    {
+        public Task Handle(OrderUpdatedEvent message, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(OrderRegisteredEvent message, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(OrderRemovedEvent message, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
