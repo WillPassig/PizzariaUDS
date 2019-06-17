acd095d6333a53e0460bf094c371da4b20b34d3c
Passig <wilpsouza@gmail.com>
2019-06-16 23:02:39 -0300

    Foram corrigidos os arquivos appsettings.json

--- PizzariaUDS/PizzariaUDS.Infra.Data/Context/PizzariaUDSContext.cs
+++ PizzariaUDS/PizzariaUDS.Infra.Data/Context/PizzariaUDSContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using PizzariaUDS.Domain.Models;
